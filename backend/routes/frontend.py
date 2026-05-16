5213b58e343c472c80db4b9ff44bfe604d8ed7d7
Supratim Bajpai <supratimbajpai59@gmail.com>
2026-05-16 14:37:06 +0530

    Delete backend directory

--- backend/routes/frontend.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-frontend_bp = Blueprint('frontend', __name__)
-
-
-@frontend_bp.route('/')
-@frontend_bp.route('/<path:path>')
-def index(path=''):
-    return render_template('index.html')
